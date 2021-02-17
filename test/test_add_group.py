e9543b28a2da444459ea30f90a16ce0e654e46c2
Chunikhina <chunikhina@reksoft.ru>
2021-02-17 09:18:47 +0300

    add GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,12 +11,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="trytytry", header="rytryrtyrty", footer="rytryrtyrtyrty"))
-    app.return_to_groups_page()
+    app.group.create(Group(name="trytytry", header="rytryrtyrty", footer="rytryrtyrtyrty"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.return_to_groups_page()
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
