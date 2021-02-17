e7e8677427476eebf642a7a81d66bbdd9b8b4a89
Chunikhina <chunikhina@reksoft.ru>
2021-02-17 09:03:53 +0300

    add SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,13 +10,13 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="trytytry", header="rytryrtyrty", footer="rytryrtyrtyrty"))
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
