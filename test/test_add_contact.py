1eea9d47ef1ec0cce9b8062230ef6009da394633
Chunikhina <chunikhina@reksoft.ru>
2021-02-17 15:52:03 +0300

    add conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from fixture.application import Application
-import pytest
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     new_contact = Contact(firstname="asdfg", middlename="asdfg", lastname="asdfg", nickname="asdfg", title="asdfg",
---
+++ test/test_del_group.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+from fixture.application import Application
+import pytest
+
+def test_delete_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.delete_first_group()
+    app.session.logout()
+
+
