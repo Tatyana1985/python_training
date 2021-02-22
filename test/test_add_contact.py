435a5df75672beb34e6debd5aedece23aa38ce91
Chunikhina <chunikhina@reksoft.ru>
2021-02-22 23:51:00 +0300

    add fixture validation add a check for the current state of the session

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,9 +7,8 @@ def test_add_contact(app):
                           home="565656", mobile="677565", work="76876687", fax="67678678", email="rert@uyuy.try",
                           email2="rtrt@tyty.tyty",
                           email3="rtr@uiui.tyt", homepage="trtr@yuu.uyu", day="7", month="August", year="2000")
-    app.session.login(username="admin", password="secret")
     app.contact.add_contact(new_contact)
-    app.session.logout()
+
 
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,4 @@ from model.group import Group
 
 def test_edit_first_group(app):
     edit_group = Group(name="erwew", header="eqweq", footer="ouiou")
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(edit_group)
-    app.session.logout()
